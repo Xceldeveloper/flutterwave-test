2eaa6741b516fc96d16b6f8127691b0377069ca6
Xcedeveloper <xceldeveloper@gmail.com>
2022-01-23 04:51:52 +0100

    change

--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,27 @@
   <div class="page">
    <featured-post/>
    <post-lists/>
+    <observer @on-change="onChange">
+      </observer>
   </div>
 </template>
 
 <script>
 import PostLists from '~/components/custom/post-lists.vue'
 import featuredPost from '~/components/views/featured-post.vue'
+import Observer from 'vue-intersection-observer'
 export default {
-  components: { featuredPost, PostLists },
+  components: { featuredPost, PostLists,Observer },
   created(){
     // fetch posts here even before the component is counted 
       this.$store.dispatch("posts/getAllPosts")
    },
+   methods: {
+     onChange(entry, obv) {
+      if(this.$store.state.posts.posts.length > 0 ){
+        this.$store.dispatch('posts/loadMorePosts',this.$store.state.posts.posts.length+1)
+      }
+     }}
 
 }
 </script>
